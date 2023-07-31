e3ca37079db412ad4d3aec0e62066797d3208476
charuka deshan <itscharukadeshan@gmail.com>
2023-07-31 10:15:21 +0530

    add project data fields

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -19,10 +19,39 @@ const ClientType = new GraphQLObjectType({
     phone: { type: GraphQLString },
   }),
 });
+const ProjectType = new GraphQLObjectType({
+  name: "Project",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+    client: {
+      type: ClientType,
+      resolve(parent, arg) {
+        return clients.find((client) => client.id === parent.clientId);
+      },
+    },
+  }),
+});
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    projects: {
+      type: GraphQLList(ProjectType),
+
+      resolve(parent, arg) {
+        return projects;
+      },
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, arg) {
+        return projects.find((project) => project.id === arg.id);
+      },
+    },
     clients: {
       type: GraphQLList(ClientType),
 
