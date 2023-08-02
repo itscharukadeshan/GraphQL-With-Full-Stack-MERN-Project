10cc752891c399a69e54517af3149fa812023674
charuka deshan <itscharukadeshan@gmail.com>
2023-08-02 20:21:07 +0530

    add hover effect

--- client/src/components/ProjectCard.jsx
+++ client/src/components/ProjectCard.jsx
@@ -1,11 +1,13 @@
 /** @format */
+import { FaExternalLinkAlt } from "react-icons/fa";
 
 function ProjectCard({ project }) {
-  console.log(project);
   return (
-    <div className='card card-side bg-base-300 shadow-xl ml-5'>
+    <div className='card card-side bg-base-300 shadow-xl ml-5 '>
       <div className='card-body'>
-        <h2 className='card-title'>{project.name}</h2>
+        <h2 className='card-title hover:text-warning '>
+          {project.name} <FaExternalLinkAlt className='w-4 mt-2 h-full' />
+        </h2>
         <p className=' w-50 h-fit py-2'>{project.description}</p>
         <div className='card-actions justify-end'>
           <div className='badge badge-warning badge-outline mt-4'>
