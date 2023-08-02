14b2726583c08be63d247dfaf52cd59b347ba4c3
charuka deshan <itscharukadeshan@gmail.com>
2023-08-02 11:23:47 +0530

    add delete mutation

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,14 +1,20 @@
 /** @format */
 import { FaTrash } from "react-icons/fa";
+import { useMutation } from "@apollo/client";
+import { DELETE_CLIENT } from "../mutations/clientMutations";
 
 function ClientRow({ client }) {
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: { id: client.id },
+  });
+
   return (
     <tr>
       <td>{client.name}</td>
       <td>{client.email}</td>
       <td>{client.phone}</td>
       <td>
-        <button className='btn btn-sm btn-warning'>
+        <button onClick={deleteClient} className='btn btn-sm btn-warning'>
           <FaTrash />
           Delete
         </button>
