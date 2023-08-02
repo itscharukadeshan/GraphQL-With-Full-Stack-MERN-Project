a2fbe8933b461dacf9a0df15dd54ab101b9679e2
charuka deshan <itscharukadeshan@gmail.com>
2023-08-02 12:12:06 +0530

    handle the cache data

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,6 +7,25 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 import "./index.css";
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
 const client = new ApolloClient({
   uri: "http://localhost:3000/graphql",
   cache: new InMemoryCache(),
