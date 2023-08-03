82ba2d963749eb727a8db5fcfe89050a702fbe66
charuka deshan <itscharukadeshan@gmail.com>
2023-08-03 11:42:33 +0530

    add link to have in nav

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,10 +1,10 @@
 /** @format */
-
+import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <div className='navbar bg-gray-700 shadow-md '>
       <a className='btn btn-ghost normal-case text-xl text-pink-400 font-mono'>
-        Graphql MERN
+        <Link to={"/"}>Graphql MERN</Link>
       </a>
     </div>
   );
